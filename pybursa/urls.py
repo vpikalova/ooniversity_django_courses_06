b07383c73bbb7982314706b51afe0d34b1427806
KonstRk <qasenior32@gmail.com>
2017-03-18 17:03:11 +0200

    Control task3 2 test 2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -5,11 +5,11 @@ from django.http import HttpResponse
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'index', views.index),
-    url(r'contact', views.contact),
-    url(r'student_list', views.student_list),
-    url(r'student_detail', views.student_detail),
+    url(r'^$', views.index, name ="index"),
+    url(r'index', views.index, name ="index"),
+    url(r'contact', views.contact, name = "contact"),
+    url(r'student_list', views.student_list, name = "student_list"),
+    url(r'student_detail', views.student_detail, name = "student_detail"),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
