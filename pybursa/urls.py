cd1902e8b3bace8ccf4beb4163c875f820dcb953
KonstRk <qasenior32@gmail.com>
2017-03-18 16:23:53 +0200

    Control task3 2 test 2

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -2,26 +2,14 @@ from django.conf.urls import include, url
 from django.contrib import admin
 from django.shortcuts import render
 from django.http import HttpResponse
-
-def indexpage(request):
-    return render(request, "index.html")
-
-def contactpage(request):
-    return render(request, "contact.html")
-
-def student_list(request):
-    return render(request, "student_list.html")
-
-def student_detail(request):
-    return render(request, "student_detail.html")
-
+from . import views
 
 urlpatterns = [
-    url(r'^$', indexpage),
-    url(r'index', indexpage),
-    url(r'contact', contactpage),
-    url(r'student_list', student_list),
-    url(r'student_detail', student_detail),
+    url(r'^$', views.index),
+    url(r'index', views.index),
+    url(r'contact', views.contact),
+    url(r'student_list', views.student_list),
+    url(r'student_detail', views.student_detail),
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
 ]
