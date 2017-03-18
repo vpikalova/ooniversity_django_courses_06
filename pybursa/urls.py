41192d1ea62d416ccaec63b655f00608b84ab60c
Eduard Striukov <edstrkv@gmail.com>
2017-03-18 09:39:38 +0200

    week 2: kz 3.2.1

--- pybursa/urls.py
+++ pybursa/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from .views import index, contact, student_list, student_detail
 
 urlpatterns = [
     url(r'^polls/', include('polls.urls')),
     url(r'^admin/', admin.site.urls),
+
+    url(r'^$', index, name='index'),
+    url(r'^contact/$', contact, name='contact'),
+    url(r'^student_list/$', student_list, name='student_list'),
+    url(r'^student_detail/$', student_detail, name='student_detail'),
 ]
