0332c4b5b38e46a5aa98087187f7be0389790dc4
Eduard Striukov <edstrkv@gmail.com>
2017-03-18 22:16:11 +0200

    week 2: hw 4.1

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'quadratic.apps.QuadraticConfig',
     'polls.apps.PollsConfig', 
     'django.contrib.admin',
     'django.contrib.auth',
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'pybursa.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
---
+++ quadratic/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render
+
+
+def validation(value, first_value=False):
+    value_error = ''
+    not_int_error = 'коэффициент не целое число'
+    none_error = 'коэффициент не определен'
+    first_value_zero_error = 'коэффициент при первом слагаемом\
+                              уравнения не может быть равным нулю'
+    try:
+        if len(value) == 0:
+            value_error = none_error
+        elif int(value) == 0 and first_value is True:
+            value_error = first_value_zero_error
+    except ValueError:
+        value_error = not_int_error
+    return(value_error)
+
+def calc_root(order = 1):
+    if order == 1:
+        x = (-int(b) + int(d) ** (1 / 2)) / 2 * int(a)
+    else:
+        x = (-int(b) - int(d) ** (1 / 2)) / 2 * int(a)
+    return x
+
+def quadratic_results(request):
+    a = request.GET['a']
+    b = request.GET['b']
+    c = request.GET['c']
+    error_a = validation(request.GET['a'], True)
+    error_b = validation(request.GET['b'])
+    error_c = validation(request.GET['c'])
+    flag = False
+    descr = False
+    roots = ''
+    info = ''
+    x1 = ''
+    x2 = ''
+    if error_a == '' and error_b == '' and error_c == '':
+        flag = True
+        descr = int(b) ** 2 - 4 * int(a) * int(c)
+        if descr < 0:
+            info = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
+        elif descr == 0:
+            x1 = x2 = (-int(b) + int(descr) ** (1 / 2)) / 2 * int(a)
+            info = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x1
+        elif descr > 0:
+            x1 = (-int(b) + int(descr) ** (1 / 2)) / 2 * int(a)
+            x2 = (-int(b) - int(descr) ** (1 / 2)) / 2 * int(a)
+            info = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
+
+    context = {'a': a, 'b': b, 'c': c, 'error_a': error_a, 'error_b': error_b, 'error_c': error_c, 'descr': descr, 'roots': roots, 'flag' : flag, 'info': info}
+    return render(request, 'quadratic/results.html', context)
+
+
