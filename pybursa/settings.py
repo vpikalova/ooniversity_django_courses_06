6e7f7018e5065d87c4cf117b5258a46a2993f1f5
mazart <am.magicsmile@gmail.com>
2017-03-19 21:49:06 +0200

    views file is copy

--- pybursa/settings.py
+++ pybursa/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'polls.apps.PollsConfig',
-	'quadratic',
+	'quadratic.apps.QuadraticConfig',
 ]
 
 MIDDLEWARE = [
--- quadratic/urls.py
+++ quadratic/urls.py
@@ -1,15 +1,8 @@
-from django.conf.urls import include, url
-from django.contrib import admin
-from .views import index, contact, student_list, student_detail
+from django.conf.urls import url
+
 from . import views
 
+#app_name = 'quadratic'
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-	url(r'^$', index, name='index'),
-	url(r'^contact/$', contact, name='contact'),
-	url(r'^student_list/$', student_list, name='student_list'),
-	url(r'^student_detail/$', student_detail, name='student_detail'),
-	url(r'', include('quadratic.urls')),
-	url(r'^$', views.quadratic_results, name='results')
-	url(r'^quadratic/results/(?P<c>\d+)$', views.quadratic_results, name='results'), # ?a=(?P<a>\d+)&b=(?P<b>\d+)&c=
+    url(r'^result/$', views.quadratic_results, name='results'),
 ]
