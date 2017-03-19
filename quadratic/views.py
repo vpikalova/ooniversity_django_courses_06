2bd4a1e2d93a9e2177d9b35c8e571264ec2f3081
Roman <hardworkerst@gmail.com>
2017-03-19 20:03:40 +0200

    views.py edit

--- quadratic/views.py
+++ quadratic/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 
 MSG_NOT_DEFINED = 'коэффициент не определен'
 MSG_NOT_INT = 'коэффициент не целое число'
+MSG_A_IS_ZERO = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
 
 
 def quadratic_results(request):
@@ -17,29 +18,30 @@ def quadratic_results(request):
         if vars[key] != "":
             try:
                 vars[key] = int(vars[key])
+                print(vars[key])
+                if key == 'a' and vars[key] == 0:
+                    vars[key] = str(vars[key]) + "<br /><i>%s</i>" % MSG_A_IS_ZERO
+                    errors = True
             except ValueError:
-                vars[key] = vars[key] + "<br />" + MSG_NOT_INT
+                vars[key] = vars[key] + "<br /><i>%s</i>" % MSG_NOT_INT
                 errors = True
         else:
-            vars[key] = vars[key] + "<br />" + MSG_NOT_DEFINED
+            vars[key] = vars[key] + "<br /><i>%s</i>" % MSG_NOT_DEFINED
             errors = True
     if not errors:
         a = vars['a']
         b = vars['b']
         c = vars['c']
         message = str()
-        if a == 0:
-            message = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
+        d = b ** 2 - 4 * a * c
+        vars['d'] = d
+        if d >= 0:
+            x1 = (-b + math.sqrt(d)) / 2 * a
+            x2 = (-b - math.sqrt(d)) / 2 * a
+            message = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: " \
+                      "x1 = x2 = %s" % x1 if d == 0 else \
+                      "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" % (x1, x2)
         else:
-            d = b ** 2 - 4 * a * c
-            vars['d'] = d
-            if d >= 0:
-                x1 = (-b + math.sqrt(d)) / 2 * a
-                x2 = (-b - math.sqrt(d)) / 2 * a
-                message = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: " \
-                          "x1 = x2 = %s" % x1 if d == 0 else \
-                          "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" % (x1, x2)
-            else:
-                message = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
+            message = 'Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений.'
         vars['message'] = message
     return render(request, 'results.html', vars)
