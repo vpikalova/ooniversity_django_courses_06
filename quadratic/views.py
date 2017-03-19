fc1f8b381f6ecbd22ed37fa925539d8c220d0967
Eduard Striukov <edstrkv@gmail.com>
2017-03-19 22:40:24 +0200

    week 2, hw 4.5

--- quadratic/views.py
+++ quadratic/views.py
@@ -5,8 +5,7 @@ def validation(value, first_value=False):
     value_error = ''
     not_int_error = 'коэффициент не целое число'
     none_error = 'коэффициент не определен'
-    first_value_zero_error = 'коэффициент при первом слагаемом\
-                              уравнения не может быть равным нулю'
+    first_value_zero_error = 'коэффициент при первом слагаемом уравнения не может быть равным нулю'
     try:
         if len(value) == 0:
             value_error = none_error
@@ -31,24 +30,24 @@ def quadratic_results(request):
     error_b = validation(request.GET['b'])
     error_c = validation(request.GET['c'])
     flag = False
-    descr = False
+    diskr = None
     messages = ''
     x1 = ''
     x2 = ''
     if error_a == '' and error_b == '' and error_c == '':
         flag = True
-        descr = int(b) ** 2 - 4 * int(a) * int(c)
-        if descr < 0:
+        diskr = int(b) ** 2 - 4 * int(a) * int(c)
+        if diskr < 0:
             messages = "Дискриминант меньше нуля, квадратное уравнение не имеет действительных решений."
-        elif descr == 0:
-            x1 = x2 = (-int(b) + int(descr) ** (1 / 2)) / 2 * int(a)
+        elif diskr == 0:
+            x1 = x2 = (-int(b) + int(diskr) ** (1 / 2)) / 2 * int(a)
             messages = "Дискриминант равен нулю, квадратное уравнение имеет один действительный корень: x1 = x2 = %s" %x1
-        elif descr > 0:
-            x1 = (-int(b) + int(descr) ** (1 / 2)) / 2 * int(a)
-            x2 = (-int(b) - int(descr) ** (1 / 2)) / 2 * int(a)
+        elif diskr > 0:
+            x1 = (-int(b) + int(diskr) ** (1 / 2)) / 2 * int(a)
+            x2 = (-int(b) - int(diskr) ** (1 / 2)) / 2 * int(a)
             messages = "Квадратное уравнение имеет два действительных корня: x1 = %s, x2 = %s" %(x1, x2)     
 
-    context = {'a': a, 'b': b, 'c': c, 'error_a': error_a, 'error_b': error_b, 'error_c': error_c, 'descr': descr, 'flag' : flag, 'messages': messages}
+    context = {'a': a, 'b': b, 'c': c, 'error_a': error_a, 'error_b': error_b, 'error_c': error_c, 'diskr': diskr, 'flag' : flag, 'messages': messages}
     return render(request, 'quadratic/results.html', context)
 
 
